cec66106930d18d14679b9e999d110a3be2163c8
jcruzpalermo <jcruzpalermo@gmail.com>
2022-08-19 09:38:32 -0300

    corregido

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,10 +1,8 @@
 import './item.css';
 
 import { Link } from 'react-router-dom';
-import React, {useContext} from 'react';
-import { CartContext } from '../../context/CartContext';
 const Item = ({ info }) => {
-    const nombre = useContext(CartContext);
+    
     return (
         <Link to={`/detalle/${info.id}`} className='foto'>
             <img src={info.image} alt="Fuego" />
